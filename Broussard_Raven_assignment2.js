d194b267afe0844a05e9ad8fcc549a2702322c00
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-12-03 18:50:38 -0600

    variables
    adding initial variables

--- Broussard_Raven_assignment2.js
+++ Broussard_Raven_assignment2.js
@@ -1,1 +1,14 @@
-alert("JavaScript works!");
+//  Raven "Nikki" Broussard
+//  SDI Project 2
+//  December 3, 2014
+
+//  Variables
+
+var customer = "John";
+var simCard = "Net10";
+var cdma = "Page Plus";
+
+
+console.log( customer )
+console.log( simCard )
+console.log( cdma )
