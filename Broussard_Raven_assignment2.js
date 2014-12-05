c93101293a3aa7c969906a2411a144224f94a374
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-12-04 22:31:54 -0600

    boolean

--- Broussard_Raven_assignment2.js
+++ Broussard_Raven_assignment2.js
@@ -12,3 +12,30 @@ var cdma = "Page Plus";
 console.log( customer )
 console.log( simCard )
 console.log( cdma )
+
+// Procedure
+var moneySaved = function (bank) {
+	if (bank < 100) {
+		console.log("I can't save you any real money today!");
+		}
+		else {
+		console.log("Good News! I can save you loads of money!");
+		};
+		
+		return;
+};
+
+//  Boolean Function
+var sellPhone = function (savesMoney,justService) {
+		while (savesMoney) {
+			if (bank < 100){
+				bank ++;
+			console.log("Awesome, " + customer + " you now have saved " + bank + " dollars!");
+			console.log("let's get your service started");
+		} else {
+			console.log("Let's get you a new phone and some start savings.");
+		needPhone = false;
+		};
+	};
+	return true;
+};
