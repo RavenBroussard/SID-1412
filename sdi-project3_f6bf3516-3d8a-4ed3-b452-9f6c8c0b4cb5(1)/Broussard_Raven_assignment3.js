10736bd7ff8225411df53d158388b3bc4ae3507f
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-12-11 17:55:07 -0600

    json data

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5(1)/Broussard_Raven_assignment3.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5(1)/Broussard_Raven_assignment3.js
@@ -1,1 +1,32 @@
-alert("JavaScript works!");
+//  alert("JavaScript works!");
+
+//  Raven "Nikki" Broussard
+//  SDI 1412  Project 3
+//  December 11, 2014
+
+//  variables
+
+var customerName = "John";
+var store = "Wilson's Electronics";
+var money = 800;
+var whatCustomerBuys = ["1 Samsung Phone", "1 activation & month of service", "2 chargers", "2 cases"];
+var inventory = {
+	"equipment": "Samsung Phone",
+	"protection": "cases",
+	"plan": "activation & month of service",
+	"power": "chargers"
+	};
+
+console.log(customerName + " enters " + store + " to get set up with a new phone and new service." )
+console.log(customerName + " has a budget of " + money + " to spend today.")
+console.log("If, " + customerName + " has money left over he will buy more airtime.")
+
+// Json 
+
+var handleData = function(json){
+	for (var i = 0; i <json.purchase.length; i++) {
+	var purchase = jason.purchase[i];
+		console.log("Equipment: " + purchase.equipment + ", Type: " + purchase.type + ", Quantity: " + purchase.quantity);
+		};
+	};
+
---
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5(1)/json.js
@@ -0,0 +1,25 @@
+//
+var json = {
+	"Purchase" : [
+		{	"equipment": "Phone",
+			"type": "Samsung Phone",
+			"quantity": 1
+			},
+		{	"equipment": "Protection",
+			"type": "case",
+			"quantity": 2
+			},	
+		{	"equipment": "Plan",
+			"type": "activation & month of service",
+			"quantity": 1
+			},
+		{	"equipment": "Power",
+			"type": "charger",
+			"quantity": 2
+			},		
+			
+			
+			
+			
+			
+	
