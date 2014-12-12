e01f929b78b340250d0b6c7cbc17f9bf5ebbcdbd
RNBroussard@fullsail.edu <RNBroussard@fullsail.edu>
2014-12-11 20:28:16 -0600

    boolean

--- sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5(1)/Broussard_Raven_assignment3.js
+++ sdi-project3_f6bf3516-3d8a-4ed3-b452-9f6c8c0b4cb5(1)/Broussard_Raven_assignment3.js
@@ -9,6 +9,7 @@
 var customerName = "John";
 var store = "Wilson's Electronics";
 var money = 800;
+var totalSpent = 625
 var whatCustomerBuys = ["1 Samsung Phone", "1 activation & month of service", "2 chargers", "2 cases"];
 var inventory = {
 	"equipment": "Samsung Phone",
@@ -52,3 +53,50 @@ console.log("I have " + purchase[i] + " things to pick up!");
 };
 return "Leaving the store will all I came for.";
 };
+
+//  Procedure
+
+// Procedure
+var subtraction = function(money, totalSpent) {
+var moneyLeftOver = money-totalSpent;
+if (totalSpent < money) {
+console.log(customerName + " has $" + moneyLeftOver + " left over after shopping to buy airtime!");
+} else {
+console.log(customerName + " has no money left over to buy airtime.");
+};
+};
+subtraction(800,600);
+
+// Booleans
+
+var cost = itemsCost[17,480,66,10];
+var inventoryCost = function ([cases, phone, activation, chargers]) {
+var totalCost = (cases+cases)+phone+activation+(chargers+chargers);
+var moneyLeft = money-totalCost;
+	if ((totalCost || moneyLeft) < money) {
+console.log("It is true that " + customerName + " has money left over for a airtime.");
+	return true;
+		} else {
+	console.log("It is false, " + customerName + " has no money left over for a airtime.");
+		return false;
+	};
+ 
+
+// Number Function
+
+
+var spend = function(airtimeMoney) {
+	while (airtimeMoney > 0) {
+console.log(customerName + " has " + airtimeMoney + " left to buy airtime!");
+	airtimeMoney--;
+var airtimeLeft = 11;
+	while (airtimeLeft > 0) {
+		console.log(customerName + " can get " + airtimeLeft + " airtime!");
+			airtimeLeft--;
+		};
+	};
+	console.log(customerName + " has no more money to buy airtime!");
+return 0;
+};
+var allGone = spend(600);}
+
